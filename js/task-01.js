478f0149b70eaed46f2beae4a8e91b382b621f74
TkachikNataly <tkachik.nataly@gmail.com>
2022-01-14 00:53:21 +0200

    task-01

--- js/task-01.js
+++ js/task-01.js
@@ -1,26 +1,9 @@
-// const categories = document.querySelectorAll("#categories li.item");
-// const numberOfCategories = categories.length;
-// console.log(`Number of categories: ${numberOfCategories}`);
-
-// // change NodeList in Array
-// const categoryArr = [...categories];
-// const headingTextArray = categoryArr.map((item) => {
-//   const text = item.querySelector("h2").textContent;
-//   const elements = item.querySelector("ul").children.length;
-//   console.log(`Category: ${text}`);
-//   console.log(`Numbers elements: ${elements}`);
-// });
-
-// console.dir(Document);
 
 const categories = document.querySelectorAll("ul#categories li.item");
-const numberCategories = categories.length;
-console.log(`Number of categories: ${numberCategories}`);
-// console.log(categories);
-// const title = categories.querySelectorAll("h2");
-// console.log(title.textContent);
+// const numberCategories = categories.length;
+console.log(`Number of categories: ${categories.length}`);
+categories.forEach((element) => {
+    console.log(`Category: ${element.querySelector("h2").textContent} `);
+    console.log(`Elements: ${element.querySelectorAll("li").length}`);
 
-categoriesEllement => {
-    const title = li.querySelectorAll("h2");
-    console.log(title).textContent;
-}
+});
