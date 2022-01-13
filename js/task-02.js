23a2951a2a1510c4c1cfb8332e3187aeb0809842
TkachikNataly <tkachik.nataly@gmail.com>
2022-01-14 01:45:03 +0200

    Task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,10 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const listNew = document.querySelector("#ingredients");
+ingredients.forEach((element) => {
+  const elementLiNew = document.createElement("li");
+  elementLiNew.textContent = element;
+  elementLiNew.classList.add("item");
+  listNew.append(elementLiNew);
+});
