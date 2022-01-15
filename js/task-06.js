f03f28ab1f891ac1790b9c62199796db11f586d7
TkachikNataly <tkachik.nataly@gmail.com>
2022-01-16 00:21:01 +0200

    task - 06

---
+++ js/task-06.js
@@ -0,0 +1,16 @@
+const elInput = document.querySelector("#validation-input");
+
+const noBlur = (event) => {
+    if (event.currentTarget.value.length >= Number(elInput.dataset.length)) {
+        
+        elInput.classList.add('valid');
+        elInput.classList.remove('invalid');
+    }
+    else {
+        
+        elInput.classList.add('invalid');
+        elInput.classList.remove('valid');
+    }
+};
+elInput.addEventListener('blur', noBlur);
+
