3588b1d90f6f0577d96b27daed0283b3d90972cc
TkachikNataly <tkachik.nataly@gmail.com>
2022-01-16 02:35:44 +0200

    task - 09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const colorButton = document.querySelector(".change-color");
+const colorSpain = document.querySelector(".color");
+
+const changeColor = () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+};
+
+const showSpain = () => {
+  showSpain.textContent = getRandomHexColor();
+}
+
+colorButton.addEventListener("click", changeColor);
+colorSpain.addEventListener("click", showSpain);
